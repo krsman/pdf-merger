36e872f3be1fa4f998f1469c13d0a614eeae6b9c
Vladimir Krsmanovic <krsman@gmail.com>
2019-08-07 10:24:37 +0200

    updated package to use setassign namespace

--- src/PDFMerger/PDFMerger.php
+++ src/PDFMerger/PDFMerger.php
@@ -24,8 +24,6 @@
 namespace Clegginabox\PDFMerger;
 
 use Exception;
-use fpdi\FPDI;
-use fpdf\FPDF;
 
 class PDFMerger
 {
@@ -66,7 +64,7 @@ class PDFMerger
             throw new Exception("No PDFs to merge.");
         }
 
-        $fpdi = new FPDI;
+        $fpdi = new \setasign\Fpdi\Fpdi();
 
         // merger operations
         foreach ($this->_files as $file) {
@@ -82,7 +80,7 @@ class PDFMerger
                     $template   = $fpdi->importPage($i);
                     $size       = $fpdi->getTemplateSize($template);
 
-                    $fpdi->AddPage($fileorientation, array($size['w'], $size['h']));
+                    $fpdi->AddPage($fileorientation, array($size['width'], $size['height']));
                     $fpdi->useTemplate($template);
                 }
             } else {
@@ -92,7 +90,7 @@ class PDFMerger
                     }
                     $size = $fpdi->getTemplateSize($template);
 
-                    $fpdi->AddPage($fileorientation, array($size['w'], $size['h']));
+                    $fpdi->AddPage($fileorientation, array($size['width'], $size['height']));
                     $fpdi->useTemplate($template);
                 }
             }
